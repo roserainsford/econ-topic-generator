ab1893b271ee7715edd288bd95b830581a7311a4
roserainsford <rose.rainsford@ashbournecollege.co.uk>
2026-05-11 09:37:35 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: 40 }}>
+      <h1>Edexcel Economics Topic Generator</h1>
+      <p>Your app is working.</p>
+    </div>
+  );
+}
